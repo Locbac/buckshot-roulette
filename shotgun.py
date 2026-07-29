16f864160980f6dc39fee086d6f5f637d6ec6675
Tasha Sloan <locbac@national.shitposting.agency>
2026-07-29 09:54:56 -0400

    refactor(shotgun): add type hints and result type

--- shotgun.py
+++ shotgun.py
@@ -1,5 +1,6 @@
 # shotgun.py
 from enum import IntEnum
+from typing import NamedTuple
 import random
 
 class Shell(IntEnum):
@@ -7,37 +8,48 @@ class Shell(IntEnum):
     LIVE = 1
     BLANK = 2
 
+class ChamberResult(NamedTuple):
+    idx: int
+    shell: Shell
+
 class Shotgun:
-    def __init__(self, size=8):
+    def __init__(self, size: int = 8) -> None:
         self.size = size
         self.chambers = []
         self.reset()
+    
+    def reload(self) -> None:
+        self.reset()
+        self.randomize()
+        self.compact()
 
-    def reset(self):
+    def reset(self) -> None:
         # fill self.chambers with EMPTY, size long
         for chamber in range(self.size):
             self.chambers.append(Shell.EMPTY)
 
-    def randomize(self):
+    def randomize(self) -> None:
         # fill each chamber with a random Shell
         for chamber in range(self.size):
             choices = [Shell.LIVE, Shell.BLANK, Shell.EMPTY]
             self.chambers[chamber] = random.choice(choices)
 
-    def compact(self):
+    def compact(self) -> None:
         # move EMPTY chambers to the end
         non_empty = [x for x in self.chambers if x != Shell.EMPTY]
         number_to_pad_by = self.size - len(non_empty)
         self.chambers = non_empty + [Shell.EMPTY]*number_to_pad_by
 
-    def rack(self):
+    def rack(self) -> Shell | None:
         # pop/return the next chamber's shell
-        first_non_empty_chamber = self.first_non_empty(self)
-        value = self.chambers[first_non_empty_chamber]
-        self.chambers[first_non_empty_chamber] = Shell.EMPTY
-        return value
+        _first_non_empty = self.first_non_empty()
+        if _first_non_empty is None:
+            return
+        else: 
+            self.chambers[_first_non_empty.idx] = Shell.EMPTY
+            return _first_non_empty.shell
 
-    def live_count(self):
+    def live_count(self) -> int:
         count = 0
         for chamber in range(self.size):
             if self.chambers[chamber] == Shell.LIVE:
@@ -45,7 +57,7 @@ class Shotgun:
         return count
             
 
-    def blank_count(self):
+    def blank_count(self) -> int:
         count = 0
         for chamber in range(self.size):
             if self.chambers[chamber] == Shell.BLANK:
@@ -53,17 +65,19 @@ class Shotgun:
         return count
 
 
-    def empty_count(self):
+    def empty_count(self) -> int:
         count = 0
         for chamber in range(self.size):
             if self.chambers[chamber] == Shell.EMPTY:
                 count += 1
         return count
 
-    def first_non_empty(self):
+    def first_non_empty(self) -> ChamberResult | None:
         for chamber in range(self.size):
-            if chamber != Shell.EMPTY:
-                return chamber
+            shell = self.chambers[chamber]
+            if shell != Shell.EMPTY:
+                return ChamberResult(chamber, shell)
+        return None
 
     def __repr__(self):
         return f"Shotgun({self.chambers})"
