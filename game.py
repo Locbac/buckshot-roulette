2a6b74188a397e2cbb6bc920a8ef47d16e927a20
Dr Vre <vladimirritz2023@gmail.com>
2026-07-30 14:13:04 -0400

    Organized code ! !

--- game.py
+++ game.py
@@ -1,9 +1,39 @@
 from player import Player
+import random # new, moved from main
+
+
+class Player_Settings: #new, I decided to move the game settings to our game.py so main can be reserved for executing the code
+    players = []
+    player_count = 2
+    player_health: int = random.randint(3,6)
+
+    game_settings = f"""\nplayer_count: {player_count} \nplayer_health: {player_health}\n"""
+
+
+
+
 
 class Game:
     def __init__(self, players: list[Player]) -> None:
         self.players = players
-        
+
+
+class Turn: # new
+    def __init__(self, playercount):
+        self.playercount = playercount
+        for players in range(playercount):
+            pass
+
+
+                
+    
+
+
+
+
+
+
+
 
 
         
--- main.py
+++ main.py
@@ -2,20 +2,31 @@
 from shotgun import Shotgun
 from player import Player
 from game import Game
-import random
+from game import Player_Settings #new
+from game import Turn #new
+
 
 def main():
-    players = []
-    player_count = 2
+   
     
-    player_health: int = random.randint(3,6)
-    
-    for i in range(player_count):
-        players.append(Player(total_health=player_health))
+    for i in range(Player_Settings.player_count): #new
+        Player_Settings.players.append(Player(total_health=Player_Settings.player_health)) #new
+
+    print(f"players: {Player_Settings.players[0].health}")
+
     shotgun = Shotgun()
     shotgun.randomize()
     shotgun.compact()
-    print(shotgun)
+
+
+    print(f"Current settings: {Player_Settings.game_settings}") #new
+
+    print("Round 1") #new, current string will be replaced with a function that will count the current round number
+
+    #print(shotgun) #new, didnt delete incase we need to read the final output of the shotgun 
+    print(f"{shotgun.live_count()} live") #new, now we can see the live and blank rounds but not the order like in buckshot
+    print(f"{shotgun.blank_count()} blank") #new
+
 
 if __name__ == "__main__":
     main()
